4275d0c2772ac228e2409b79557c5f98296c67fb
Jin <dowha132@yonsei.ac.kr>
2025-02-22 14:06:44 +0900

    2.3

---
+++ ch2/2.3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void printHI()
+{
+    std::cout << "HI" << "\n";
+}
+
+int main()
+{
+    printHI();
+    return 0;
+}
+// #include <iostream>
+// int returnFive()
+// {
+//     return 5;
+// }
+// int main()
+// {
+//     std::cout << returnFive();
+// }
