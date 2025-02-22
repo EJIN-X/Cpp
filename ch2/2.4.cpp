0aa73e404cd3fefabf89b1318ace1914e7c79801
Jin <dowha132@yonsei.ac.kr>
2025-02-22 15:46:48 +0900

    2.4

--- ch2/2.4.cpp
+++ ch2/2.4.cpp
@@ -4,10 +4,16 @@ int doubleNumber(int x)
 {
     return 2*x;
 }
+
+void showRslt(int x)
+{
+    std::cout<<"Twice the Number is : "<<doubleNumber(x);
+}
+
 int main()
 {
     std::cout<< "Type one integer : ";
     int num{};
     std::cin >> num;
-    std::cout <<"Twice the Number is : "<< doubleNumber(num);
+    showRslt(num);
 }
