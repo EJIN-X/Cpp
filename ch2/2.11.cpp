d9cf830aae730cdb6645f1f2f0f41c36367439ff
Jin <dowha132@yonsei.ac.kr>
2025-02-23 12:17:12 +0900

    2.11

---
+++ ch2/2.11.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// int add(int x ,int y);
+// int main()
+// {
+//     std::cout << add(3,4);
+//     return 0;
+// }
+
+#include <iostream>
+#include "add.h"
+// int add(int x ,int y);
+int main()
+{
+    std::cout << add(3,4);
+    return 0;
+}
