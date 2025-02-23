5a367cb0e8dc5211b3f963ff6e751ea51a48081d
Jin <dowha132@yonsei.ac.kr>
2025-02-23 11:35:19 +0900

    2.10

---
+++ ch2/2.10.cpp
@@ -0,0 +1,39 @@
+// #include <iostream>
+// #define MY_NAME "JIN"
+// int main()
+// {
+//     std::cout << "My name is "<< MY_NAME <<'\n';
+//     return 0;
+// }
+
+// #include <iostream>
+// #define print_JIN
+//
+// int main()
+// {
+// #ifdef print_JIN
+//     std::cout << "HI my name is Jin";
+// #endif
+// #ifdef print_noone
+//     std::cout << "Hi my name is undefined";
+// #endif
+// #ifndef print_JIN
+//     std::cout << "WHAT??";
+// #endif
+// #if 0
+//     std::cout << "NO compile";
+// #endif
+// #if 1
+//     std::cout << "Yes compile";
+// #endif
+//     return 0;
+// }
+
+#include <iostream>
+#include "JIN.h"
+
+int main()
+{
+    std::cout << "My name is " << MY_NAME << "!!";
+    return 0;
+}
