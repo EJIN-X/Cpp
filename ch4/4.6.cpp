cc73963735004d849c9547bf9fb748d24eefbde3
Jin <dowha132@yonsei.ac.kr>
2025-02-25 17:02:53 +0900

    4.6

---
+++ ch4/4.6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdint>
+// int main()
+// {
+//     int x {32767};
+//     x=x+1;
+//     std::cout << x << "\n";
+//     return 0;
+// }
+
+int main()
+{
+    std::int8_t x{65};
+    std::cout << x << '\n';
+    return 0;
+}
