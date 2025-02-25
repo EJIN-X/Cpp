a7779023bf831c5c39ff7c4ad3988eb24fbaedf5
Jin <dowha132@yonsei.ac.kr>
2025-02-25 19:45:29 +0900

    4.11

---
+++ ch4/4.11.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+// int main()
+// {
+//     char ch1{'a'};
+//     std::cout <<ch1;
+//
+//     char ch2{98};
+//     std::cout <<ch2;
+//     std::cout <<'c';
+//     return 0;
+// }
+
+// int main()
+// {
+//     std::cout << "Input a keyboard character : ";
+//     char ch{};
+//     std::cin >> ch;
+//     std::cout<<"You entered : " <<ch <<'\n';
+//     return 0;
+// }
+
+// int main()
+// {
+//     std::cout << "Input a keyboard character : ";
+//     char ch{};
+//     std::cin >> ch;
+//     std::cout<<"You entered : " <<ch <<'\n';
+//
+//     std::cin >> ch;
+//     std::cout<<"You entered : " <<ch <<'\n';
+//
+//     return 0;
+// }
+
+// int main()
+// {
+//     std::cout << "Input a keyboard character : ";
+//
+//     char ch{};
+//     std::cin.get(ch);
+//     std::cout<<"You entered : " <<ch <<'\n';
+//
+//     std::cin.get(ch);
+//     std::cout<<"You entered : " <<ch <<'\n';
+//
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x{5};
+//     std::cout << "The value of x is : "<< x << '\n';
+//     std::cout << "First line\nSecond line\n ";
+//     return 0;
+// }
+
+int main()
+{
+    int x{5};
+    std::cout <<"The value of x is "<<x<<"!\n";
+    return 0;
+}
