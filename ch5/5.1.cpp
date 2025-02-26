01be86ff2253ac1e64024425191f28ec11c47424
Jin <dowha132@yonsei.ac.kr>
2025-02-26 10:31:46 +0900

    5.1

---
+++ ch5/5.1.cpp
@@ -0,0 +1,43 @@
+// #include <iostream>
+// int main()
+// {
+//     std::cout <<"Enter your age : ";
+//     int age{};
+//     std::cin >> age;
+//
+//     const int constAge{age};
+//     age =5;
+//     constAge=6;
+//
+//
+// }
+
+// #include <iostream>
+// void printInt(const int x)
+// {
+//     std::cout <<x <<'\n';
+// }
+//
+// int main()
+// {
+//     printInt(5);
+//     printInt(6);
+//     return 0;
+// }
+
+// #include <iostream>
+// void someFcn()
+// {
+// #define gravity 9.8
+// }
+//
+// void printGravity(double gravity)
+// {
+//     std::cout <<"gravity : "<< gravity;
+// }
+// int main()
+// {
+//     printGravity(3.71);
+//     return 0;
+// }
+// //Don't use macros!
