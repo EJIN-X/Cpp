ecced60bf7bff23bdbeb09dd5413e5543b14cbb0
Jin <dowha132@yonsei.ac.kr>
2025-02-26 11:14:43 +0900

    5.2

---
+++ ch5/5.2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+// int main()
+// {
+//     std::cout <<5<<"\n";
+//     std::cout <<5L<<"\n";
+//     std::cout <<5u<<"\n";
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a{5};
+//     unsigned int b{6};
+//     long c{7};
+//     std::cout << a << b << c;
+//     return 0;
+// }
+
+int main()
+{
+    std::cout << 5.0 <<"\n";
+    std::cout <<5.0f << "\n";
+    float f{4.1f};
+    double d {4.1};
+    double pro{4.1e-19};
+    std::cout << d+f+pro;
+
+    return 0;
+}
