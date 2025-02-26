03b9d574b419da5f8c371d54db3cbbe1ada26a27
Jin <dowha132@yonsei.ac.kr>
2025-02-26 10:36:20 +0900

    5.1

--- ch5/5.1.cpp
+++ ch5/5.1.cpp
@@ -25,19 +25,19 @@
 //     return 0;
 // }
 
-// #include <iostream>
-// void someFcn()
-// {
-// #define gravity 9.8
-// }
-//
-// void printGravity(double gravity)
-// {
-//     std::cout <<"gravity : "<< gravity;
-// }
-// int main()
-// {
-//     printGravity(3.71);
-//     return 0;
-// }
-// //Don't use macros!
+#include <iostream>
+void someFcn()
+{
+#define gravity 9.8
+}
+
+void printGravity(double gravity)
+{
+    std::cout <<"gravity : "<< gravity;
+}
+int main()
+{
+    printGravity(3.71);
+    return 0;
+}
+//Don't use macros!
