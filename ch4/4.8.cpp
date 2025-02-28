5caa15454e3c944129eb5d21d727a18c134e6709
Jin <dowha132@yonsei.ac.kr>
2025-02-28 10:28:31 +0900

    4.8

--- ch4/4.8.cpp
+++ ch4/4.8.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <iomanip>
 
-// int main()
-// {
-//     std::cout <<5.0 <<'\n';
-//     std::cout <<6.7f<<'\n';
-//     std::cout <<9876543.21<<'\n';
-//     return 0;
-// }
+ int main()
+ {
+     std::cout <<5.0 <<'\n';
+     std::cout <<6.7f<<'\n';
+     std::cout <<9876543.21<<'\n';
+     return 0;
+ }
 
 int main()
 {
@@ -16,3 +16,4 @@ int main()
     std::cout <<3.333333333333333333333333333333<<'\n';
     return 0;
 }
+
