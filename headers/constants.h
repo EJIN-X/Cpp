8a0b20a2839c4c5518de0456772d74b79d781df6
Jin <dowha132@yonsei.ac.kr>
2025-03-04 20:42:24 +0900

    7.X_2

--- headers/constants.h
+++ headers/constants.h
@@ -5,6 +5,7 @@ namespace constants
     inline constexpr double pi {3.14159};
     inline constexpr double avogadro{6.0221413e23};
     inline constexpr double myGravity{9.2};
+    inline constexpr int maxClassSize{35};
 }
 
 
