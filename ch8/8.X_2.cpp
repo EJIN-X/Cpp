687738da87fce9ab378ef552140b106762f428bd
Jin <dowha132@yonsei.ac.kr>
2025-03-10 10:49:54 +0900

    8.X_2

---
+++ ch8/8.X_2.cpp
@@ -0,0 +1,55 @@
+// Make sure that assert triggers even if we compile in release mode
+#undef NDEBUG
+
+#include <cassert> // for assert
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int x)
+{
+    if (x==1||x==0)
+        return false;
+    int count {};
+    // for (int temp{2};x>=temp;++temp)
+    // {
+    //     if (x%temp==0)
+    //         ++count;
+    //
+    // }
+    for (int temp{1};std::sqrt(x)>=temp;++temp)
+    {
+        if (x%temp==0)
+            ++count;
+
+    }
+    if (count>1)
+        return false;
+
+    return true;
+    // write this function using a for loop
+}
+
+int main()
+{
+    assert(!isPrime(0)); // terminate program if isPrime(0) is true
+    assert(!isPrime(1));
+    assert(isPrime(2));  // terminate program if isPrime(2) is false
+    assert(isPrime(3));
+    assert(!isPrime(4));
+    assert(isPrime(5));
+    assert(isPrime(7));
+    assert(!isPrime(9));
+    assert(isPrime(11));
+    assert(isPrime(13));
+    assert(!isPrime(15));
+    assert(!isPrime(16));
+    assert(isPrime(17));
+    assert(isPrime(19));
+    assert(isPrime(97));
+    assert(!isPrime(99));
+    assert(isPrime(13417));
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
