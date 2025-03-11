8f7480d4c3de2c83c7c258918869a6ca2c8122be
Jin <dowha132@yonsei.ac.kr>
2025-03-11 18:20:26 +0900

    10.7

---
+++ ch10/10.7.cpp
@@ -0,0 +1,10 @@
+// #include <iostream>
+//
+// int main()
+// {
+//     using Distance = double;
+//     Distance milesToDestination {3.4};
+//     std::cout <<milesToDestination;
+//     return 0;
+// }
+
