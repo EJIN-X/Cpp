409f93a35c948bed8ad9f332bcd331997afbd926
Jin <dowha132@yonsei.ac.kr>
2025-03-15 13:59:27 +0900

    12.7

---
+++ ch12/12.7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// int main()
+// {
+//     int x{5};
+//     std::cout <<x<<std::endl;
+//     std::cout <<&x<<std::endl;//it prints the memory address
+//
+//     std::cout << *(&x)<<std::endl; // print the value at the memory address of variable x
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x{5};
+//     std::cout <<x<<std::endl;
+//     int* ptr{&x};
+//     std::cout <<*ptr<<std::endl;
+//
+//     return 0;
+// }
+
+// int main()
+// {
+//     int i{5};
+//     double d{7.0};
+//
+//     int* iPtr{&i};
+//     // int* iPtr2{&d};
+//     // double* dPtr{&i};
+//     double* dPtr2{&d};
+//
+// }
+//start with pointers and assignment
