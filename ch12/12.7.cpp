002e6ef65b79e572c6fb76a8d2bfff899062755f
Jin <dowha132@yonsei.ac.kr>
2025-03-18 17:00:06 +0900

    12.7

--- ch12/12.7.cpp
+++ ch12/12.7.cpp
@@ -31,4 +31,71 @@
 //     double* dPtr2{&d};
 //
 // }
-//start with pointers and assignment
+// int main()
+// {
+//     int x{5};
+//     int* ptr{&x};
+//     std::cout <<x<<std::endl;
+//     std::cout <<*ptr<<std::endl;
+//
+//     *ptr=6;
+//
+//     std::cout <<x<<std::endl;
+//     std::cout <<*ptr<<std::endl;
+//
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x{5};
+//     int& ref{x};
+//     int* ptr{&x};
+//
+//     std::cout <<x<<std::endl;
+//     std::cout <<ref<<std::endl;
+//     std::cout <<*ptr<<std::endl;
+//
+//     ref=6;
+//     std::cout <<x<<std::endl;
+//     std::cout <<ref<<std::endl;
+//     std::cout <<*ptr<<std::endl;
+//
+//     *ptr=7;
+//     std::cout <<x;
+//     std::cout <<ref;
+//     std::cout <<*ptr;
+// }
+
+// #include <typeinfo>
+// int main()
+// {
+//     int x{4};
+//     std::cout <<typeid(x).name()<<std::endl;
+//     std::cout <<typeid(&x).name()<<std::endl;
+// }
+
+// int main()
+// {
+//     char* chPtr{};
+//     int* iPtr{};
+//     long double* ldPtr{};
+//
+//     std::cout <<sizeof(chPtr)<<std::endl;
+//     std::cout <<sizeof(iPtr)<<std::endl;
+//     std::cout <<sizeof(ldPtr)<<std::endl;
+// }
+
+int main()
+{
+    int x{5};
+    int* ptr{&x};
+    std::cout <<*ptr<<std::endl;
+    {
+        int y{6};
+        ptr =&y;
+        std::cout <<*ptr<<std::endl;
+    }
+    std::cout <<*ptr<<std::endl;
+    return 0;
+}
